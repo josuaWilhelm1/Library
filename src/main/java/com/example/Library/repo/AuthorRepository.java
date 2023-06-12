88058dc21015e4b027e91539837c243712a3918e
Josua <82wijo1bwi@hft-stuttgart.de>
2023-06-12 10:00:23 +0200

    cleanup

--- src/main/java/com/example/Library/repo/AuthorRepository.java
+++ src/main/java/com/example/Library/repo/AuthorRepository.java
@@ -4,12 +4,5 @@ import com.example.Library.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-//    @Query(
-//            value = "SELECT * FROM Author",
-//
-//            nativeQuery = true)
-//    List<Author> customQuery();
-
 }
 
